b28bfdb6548602a1cb4f7211b7a73077975a21c6
Syeda Umme Hani <36325175+hani29@users.noreply.github.com>
2021-12-27 12:47:56 +0530

    First commit & add header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import * as React from 'react';
+import * as React from "react";
+
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Robot Market</h1>
-      {/*Add your code here*/}
+      <Header />
+      <div className="App-container">
+        <h3>Buy Your Favorite <b className="text-highlight">Robot</b> & Control EveryThing In Your <b className="text-highlight">Smart Home</b></h3>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+import React from "react";
+import logo from "./../assets/images/logo.png";
+
+export default function Header(props) {
+  return (
+    <div className="header">
+      <img src={logo} alt="robo" />
+      <h1>Robot Market</h1>
+    </div>
+  );
+}
